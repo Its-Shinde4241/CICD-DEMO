7853c022c104cb053698cf4172c0b061b72dfca0
shubhamshinde5356 <shindeshubham5356@gmail.com>
2026-06-17 11:25:23 +0530

    Update response message in root route for clarity

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send(
-    "<center>this is just simple docker and cicd demo applicaton </center>",
-  );
+  return res.send("AFTER UPDATING THE CODE NO NEED TO AUTO DEPLOY this is just simple docker and cicd demo applicaton ");
 });
 
 const port = process.env.PORT ?? 3000;
